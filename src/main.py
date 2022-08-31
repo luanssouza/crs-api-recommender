a2167f94b51725e29fe26bd1597aeef5c8a670e2
LuanSSouza <luan.souza27@hotmail.com>
2022-08-31 00:31:45 -0300

    refactoring app.py and main.py

--- src/main.py
+++ src/main.py
@@ -4,17 +4,20 @@ from random import randint
 
 from src import utils, graph, entropy
 
-def init_conversation(full_prop_graph, ratings, g_zscore, movie_rate, age, age_auth):
+def init_conversation(full_prop_graph, movie_rate, age, age_auth):
     sub_graph = full_prop_graph.copy()
 
     sub_graph = graph.remove_films_by_age(age, age_auth, movie_rate, sub_graph)
     # sub_graph['prop'].unique()
     return utils.show_props(sub_graph, 0.33), sub_graph
 
-def second_interation(sub_graph, p_chosen):
-    return utils.prop_most_pop(sub_graph, p_chosen)
+def second_interation(dialog):
+    return utils.prop_most_pop(dialog.subgraph, dialog.p_chosen)
 
-def third_interation(sub_graph, o_chosen, p_chosen, ratings):
+def third_interation(dialog):
+    sub_graph = dialog.subgraph
+    o_chosen = dialog.o_chosen
+    p_chosen = dialog.p_chosen
 
     # create vectors of movies and objects of preference and set seed and user id
     prefered_objects = [sub_graph[(sub_graph['prop'] == p_chosen) & (sub_graph['obj'] == o_chosen)]['obj_code'].unique()[0]]
@@ -22,7 +25,18 @@ def third_interation(sub_graph, o_chosen, p_chosen, ratings):
 
     return prefered_objects, prefered_prop
 
-def conversation(full_prop_graph, sub_graph, resp, g_zscore, watched, prefered_objects, prefered_prop, user_id, p_chosen, o_chosen, edgelist, bandit):
+def conversation(full_prop_graph, resp, dialog):
+
+    sub_graph = dialog.subgraph
+    g_zscore = dialog.g_zscore
+    watched = dialog.watched
+    prefered_objects = dialog.prefered_objects
+    prefered_prop = dialog.prefered_prop
+    p_chosen = dialog.p_chosen
+    o_chosen = dialog.o_chosen
+    edgelist = dialog.edgelist
+    bandit = dialog.bandit
+
 
     if len(sub_graph) == 0 or len(sub_graph.index.unique()) == 0:
         return { "response": "There are no movies that corresponds to your preferences on our database or you already watched them all"}, True, [], []
@@ -50,52 +64,37 @@ def conversation(full_prop_graph, sub_graph, resp, g_zscore, watched, prefered_o
 
         # if ask != 0 recommend movie
         else:
-            top_m = graph.order_movies_by_pagerank(sub_graph, edgelist, watched, prefered_objects, [0.8, 0.2], True)
-
-            # case if all movies with properties were recommended but no movies were accepted by user
-            if len(top_m.index) == 0:
-                return { "response": "You have already watched all the movies with the properties you liked :("}, True, [], []
-
-            rec = full_prop_graph.loc[top_m.index[0]]['title'].unique()[0]
-            m_id = str(top_m.index[0])
-            imdb_id = full_prop_graph.loc[top_m.index[0]]['imdbId'].unique()[0]
-            props = []
-
-            for i in range(0, len(prefered_prop)):
-                t = prefered_prop[i]
-                props.append({ "id": i+1, "property": str(t[0]), "object": str(t[1])})
-
-            return { "recommendation": rec, "properties": props, "ask": ask, "movie_id": m_id, "imdbId": imdb_id }, True, top_m, []
+            return recommend(full_prop_graph, dialog)
 
     else:
         return graph.shrink_graph(sub_graph, p_chosen, o_chosen), False, [], []
 
-def recommend(full_prop_graph, sub_graph, watched, prefered_objects, prefered_prop, edgelist):
-    top_m = graph.order_movies_by_pagerank(sub_graph, edgelist, watched, prefered_objects, [0.8, 0.2], True)
+def answer(full_prop_graph, resp, dialog):
 
-    # case if all movies with properties were recommended but no movies were accepted by user
-    if len(top_m.index) == 0:
-        return { "response": "You have already watched all the movies with the properties you liked :("}, True, [], []
-
-    rec = full_prop_graph.loc[top_m.index[0]]['title'].unique()[0]
-    m_id = str(top_m.index[0])
-    imdb_id = full_prop_graph.loc[top_m.index[0]]['imdbId'].unique()[0]
-    props = []
-
-    for i in range(0, len(prefered_prop)):
-        t = prefered_prop[i]
-        props.append({ "id": i+1, "property": str(t[0]), "object": str(t[1])})
-
-    return { "recommendation": rec, "properties": props, "ask": 1, "movie_id": m_id, "imdbId": imdb_id }, True, top_m, []
-
-def answer(sub_graph, ask, ans, watched, edgelist, prefered_objects, prefered_prop, top, dif_properties, full_prop_graph, user_id):
-    if ask == 0:
-        return properties(sub_graph, ans, watched, edgelist, prefered_objects, prefered_prop, top, dif_properties)
-    else:
-        return recommendation(sub_graph, ans, watched, edgelist, prefered_objects, prefered_prop, top, full_prop_graph, user_id)
+    sub_graph = dialog.subgraph
+    ask = dialog.ask
+    watched = dialog.watched
+    edgelist = dialog.edgelist
+    prefered_objects = dialog.prefered_objects
+    prefered_prop = dialog.prefered_prop
+    dif_properties = dialog.dif_properties
+    user_id = dialog.user_id
     
+    if ask == 0:
+        return properties(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, dif_properties)
+    else:
+        return recommendation(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, full_prop_graph, user_id)
 
-def properties(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, top_p, dif_properties):
+def recommend(full_prop_graph, dialog):
+    sub_graph = dialog.subgraph
+    watched = dialog.watched
+    edgelist = dialog.edgelist
+    prefered_objects = dialog.prefered_objects
+    prefered_prop = dialog.prefered_prop
+
+    return recommend_proposal(full_prop_graph, sub_graph, watched, prefered_objects, prefered_prop, edgelist) 
+
+def properties(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, dif_properties):
 
     reward = 0
 
@@ -122,7 +121,29 @@ def properties(sub_graph, resp, watched, edgelist, prefered_objects, prefered_pr
                 
     return sub_graph, True, watched, edgelist, prefered_objects, prefered_prop, reward
 
-def recommendation(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, top_m, full_prop_graph, user_id):
+def recommendation(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, full_prop_graph, user_id):
+
+    return recommendation_proposal(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, full_prop_graph, user_id)
+
+def recommend_proposal(full_prop_graph, sub_graph, watched, prefered_objects, prefered_prop, edgelist):
+    top_m = graph.order_movies_by_pagerank(sub_graph, edgelist, watched, prefered_objects, [0.8, 0.2], True)
+
+    # case if all movies with properties were recommended but no movies were accepted by user
+    if len(top_m.index) == 0:
+        return { "response": "You have already watched all the movies with the properties you liked :("}, True, [], []
+
+    rec = full_prop_graph.loc[top_m.index[0]]['title'].unique()[0]
+    m_id = str(top_m.index[0])
+    imdb_id = full_prop_graph.loc[top_m.index[0]]['imdbId'].unique()[0]
+    props = []
+
+    for i in range(0, len(prefered_prop)):
+        t = prefered_prop[i]
+        props.append({ "id": i+1, "property": str(t[0]), "object": str(t[1])})
+
+    return { "recommendation": rec, "properties": props, "ask": 1, "movie_id": m_id, "imdbId": imdb_id }, True, top_m, []
+
+def recommendation_proposal(sub_graph, resp, watched, edgelist, prefered_objects, prefered_prop, full_prop_graph, user_id):
 
     top_m = graph.order_movies_by_pagerank(sub_graph, edgelist, watched, prefered_objects, [0.8, 0.2], True)
 
